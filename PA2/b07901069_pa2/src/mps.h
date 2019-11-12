d7b6ee94d672334201b33e876dd4ef00573d17a9
MortalHappiness <b07901069@ntu.edu.tw>
2019-11-12 14:52:12 +0800

    resubmit version

--- PA2/b07901069_pa2/src/mps.h
+++ PA2/b07901069_pa2/src/mps.h
@@ -15,6 +15,7 @@ typedef std::vector<bool_array> bool_matrix;
 
 void read_file(char*, u_short_array&);
 void write_file(char*, u_short_array&, u_short_array&);
+unsigned short top_down_mps(u_short_array&, u_short_matrix&, bool_matrix&, int, int);
 void compute_mps(u_short_array&, u_short_array&, u_short_array&);
 void get_ans(u_short_array&, u_short_array&, u_short_array&, bool_matrix&, int, int);
 
