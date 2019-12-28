af378d810daf4e800dbd2ab978f7ec59555474b6
MortalHappiness <b07901069@ntu.edu.tw>
2019-12-28 19:00:46 +0800

    resubmit

--- PA3/b07901069_pa3/src/cb.cpp
+++ PA3/b07901069_pa3/src/cb.cpp
@@ -4,6 +4,32 @@
 
 // ========================================
 
+void print_node_keys(std::vector<int>& node_keys) {
+    for (int i = 0; i < node_keys.size(); ++i) {
+        std::cout << node_keys[i] << " ";
+    }
+    std::cout << "\n";
+}
+
+void print_edge_list(std::list<Edge>& edge_list, bool tab) {
+    std::list<Edge>::iterator iter;
+    for (iter = edge_list.begin(); iter != edge_list.end(); ++iter) {
+        if (tab) {
+            std::cout << "\t";
+        }
+        std::cout << "(" << (*iter).from << ", "
+                  << (*iter).to << ", "
+                  << (*iter).weight << ")\n";
+    }
+}
+
+void print_line() {
+    for (int i = 0; i < 10; ++i) {
+        std::cout << "=";
+    }
+    std::cout << "\n";
+}
+
 void cycle_break(const bool is_directed,
                  const int n_vertices,
                  const std::vector<int>& edges_from,
@@ -40,9 +66,16 @@ void cycle_break(const bool is_directed,
             node_keys[to] -= weight;
         }
 
+        // delete edges
         int idx, val, j;
         std::list<Edge>::iterator iter;
+        std::list<Edge> del_edges;
+        Set set(n_vertices); // use disjoint set to check for connectivity
         for (i = 0; i < n_vertices; ++i) {
+            // print_node_keys(node_keys);
+            // print_edge_list(edge_list, 0);
+            // print_edge_list(del_edges, 1);
+            // print_line();
             // extract maximum key
             idx = 0;
             val = INT_MIN;
@@ -62,18 +95,51 @@ void cycle_break(const bool is_directed,
                 weight = (*iter).weight;
                 if (from == idx) {
                     node_keys[to] += weight;
+                    if (set.Find(from) != set.Find(to)) {
+                        set.Union(from, to);
+                    }
                     iter = edge_list.erase(iter);
                 } else if (to == idx) {
-                    ans_edges_from.push_back(from);
-                    ans_edges_to.push_back(to);
-                    ans_weights.push_back(weight);
-                    ans_weight += weight;
+                    del_edges.push_back(Edge{from, to, weight});
                     node_keys[from] -= weight;
                     iter = edge_list.erase(iter);
                 } else {
                     ++iter;
                 }
             }
+        }
+        // if disconnected, add edges with largest weight back
+        std::list<Edge>::iterator max_iter;
+        bool is_connected;
+        while (1) {
+            is_connected = 1;
+            val = INT_MIN;
+            for (iter = del_edges.begin(); iter != del_edges.end(); ++iter) {
+                from = (*iter).from;
+                to = (*iter).to;
+                weight = (*iter).weight;
+                if (set.Find(from) == set.Find(to)) {
+                    continue;
+                }
+                is_connected = 0;
+                if (weight > val) {
+                    val = weight;
+                    max_iter = iter;
+                }
+            }
+            if (is_connected) {
+                break;
+            }
+            set.Union((*max_iter).from, (*max_iter).to);
+            del_edges.erase(max_iter);
+        }
+
+        // output deleted edges to answer
+        for (iter = del_edges.begin(); iter != del_edges.end(); ++iter) {
+            ans_edges_from.push_back((*iter).from);
+            ans_edges_to.push_back((*iter).to);
+            ans_weights.push_back((*iter).weight);
+            ans_weight += (*iter).weight;
         }
     }
 }
